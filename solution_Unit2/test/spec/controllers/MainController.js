e1e35e1559e1637fb212dcdc178db7ed68eb2cef
mspanfilov <mspanfilov@gmail.com>
2014-03-23 20:42:42 +0400

    + silution_Unit2 and unit4

---
+++ solution_Unit2/test/spec/controllers/MainController.js
@@ -0,0 +1,26 @@
+'use strict';
+
+describe('Controller: MainCtrl', function () {
+
+  // load the controller's module
+  beforeEach(module('auction'));
+
+  var MainCtrl,
+    scope;
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function ($controller, $rootScope) {
+    scope = $rootScope.$new();
+    MainCtrl = $controller('MainCtrl', {
+      $scope: scope
+    });
+  }));
+
+  it('stub test to make Karma happy', function () {
+    // Empty body.
+  });
+
+  // it('should attach a list of awesomeThings to the scope', function () {
+  //   expect(scope.awesomeThings.length).toBe(3);
+  // });
+});
