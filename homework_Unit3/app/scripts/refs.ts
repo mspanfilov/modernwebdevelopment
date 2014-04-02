29e0b29a261ea11f69053e8b83913daea8d9083a
mspanfilov <mspanfilov@gmail.com>
2014-04-02 21:49:41 +0400

    + homevork_Unit3 v0.1

---
+++ homework_Unit3/app/scripts/refs.ts
@@ -0,0 +1,7 @@
+/// <reference path="../bower_components/DefinitelyTyped/angularjs/angular.d.ts" />
+/// <reference path="../bower_components/DefinitelyTyped/angularjs/angular-route.d.ts" />
+
+//grunt-start
+/// <reference path="app.ts" />
+/// <reference path="controllers/main.ts" />
+//grunt-end
