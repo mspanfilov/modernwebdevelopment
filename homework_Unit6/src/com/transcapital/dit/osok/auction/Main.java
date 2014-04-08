9ca7f20133fa3055adb011be20d6b322608ebc94
mspanfilov <mspanfilov@gmail.com>
2014-04-08 18:59:52 +0400

    + homework_Unit6 v.0

---
+++ homework_Unit6/src/com/transcapital/dit/osok/auction/Main.java
@@ -0,0 +1,16 @@
+package com.transcapital.dit.osok.auction;
+
+/**
+ * Created by panfilov_ms on 08.04.14.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        // creating users
+
+        // creating products
+
+        // starting auction process
+
+    }
+}
---
+++ homework_Unit6/src/com/transcapital/dit/osok/auction/User.java
@@ -0,0 +1,11 @@
+package com.transcapital.dit.osok.auction;
+
+/**
+ * Created by yfain11 on 4/4/14.
+ */
+public class User {
+    public int id;
+    public String name;
+    public String email;
+    public boolean getOverbidNotifications;
+}
